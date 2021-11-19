2ed928ba1ba2e078ea50ed0516dcfbbe57b479fe
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-19 16:10:58 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
 basic.forever(function () {
-	
+    basic.showNumber(sonar.ping(
+    DigitalPin.P3,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
 })
